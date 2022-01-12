6249efa46ae3f38ae0cf130ed7efa251aff3a476
nareha <nareh.agazaryan@yahoo.com>
2022-01-12 10:34:38 -0800

    Session 1 code text update

--- session-1-intro-to-swift/Demo/hacksprint22-s1-code/hacksprint22-s1-code/ContentView.swift
+++ session-1-intro-to-swift/Demo/hacksprint22-s1-code/hacksprint22-s1-code/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("French Toast")
+            Text("I LOVE French Toast!")
                 .bold()
             Text("I")
             HStack {
