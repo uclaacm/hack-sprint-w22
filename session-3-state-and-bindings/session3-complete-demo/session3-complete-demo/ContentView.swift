fde73af37436599bf0c663e5d1fa16b8f672e148
Eugene Lo <bleugene12@g.ucla.edu>
2022-01-18 16:18:05 -0800

    Created boilerplate project

---
+++ session-3-state-and-bindings/session3-complete-demo/session3-complete-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  session3-complete-demo
+//
+//  Created by Eugene Lo on 1/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
