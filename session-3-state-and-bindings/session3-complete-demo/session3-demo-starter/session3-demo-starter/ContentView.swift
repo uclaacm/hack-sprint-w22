4dad759de2eed5f14a1f9efc00aaa5a762c63bf4
Eugene Lo <bleugene12@g.ucla.edu>
2022-01-24 00:44:52 -0800

    Added starter demo code

---
+++ session-3-state-and-bindings/session3-complete-demo/session3-demo-starter/session3-demo-starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  session3-demo-starter
+//
+//  Created by Eugene Lo on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ session-3-state-and-bindings/session3-complete-demo/session3-demo-starter/session3-demo-starter/session3_demo_starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  session3_demo_starterApp.swift
+//  session3-demo-starter
+//
+//  Created by Eugene Lo on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct session3_demo_starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
